3b03f1fefd6dc17ab53e2b895df0b07da710d813
Adam D. Barratt <adam@adam-barratt.org.uk>
2013-03-16 17:12:04 +0000

    Completer: copyright foo
    Signed-off-by: Adam D. Barratt <adam@adam-barratt.org.uk>

--- completer.py
+++ completer.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2011 Niels Thykier <niels@thykier.net>
+# Copyright (C) 2013 Adam D. Barratt <adam@adam-barratt.org.uk>
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
