cd66fc82d87b1e19da67183709b8c3f00839e3a6
Paul Wise <pabs@debian.org>
2015-11-22 20:05:25 +0800

    Do not hard-code codenames in the URL to the freeze policy
    Closes: #808031
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- britney.py
+++ britney.py
@@ -1087,7 +1087,7 @@ class Britney(object):
         # if the package is blocked, skip it
         for hint in self.hints.search('block', package=pkg, removal=True):
             excuse.addhtml("Not touching package, as requested by %s "
-                "(check https://release.debian.org/stretch/freeze_policy.html if update is needed)" % hint.user)
+                "(check https://release.debian.org/testing/freeze_policy.html if update is needed)" % hint.user)
             excuse.addhtml("Not considered")
             excuse.addreason("block")
             self.excuses.append(excuse)
@@ -1346,7 +1346,7 @@ class Britney(object):
                         excuse.addhtml("%s request by %s ignored due to version mismatch: %s" %
                                        (unblock_cmd.capitalize(), unblocks[0].user, unblocks[0].version))
                 if suite == 'unstable' or block_cmd == 'block-udeb':
-                    tooltip = "check https://release.debian.org/stretch/freeze_policy.html if update is needed"
+                    tooltip = "check https://release.debian.org/testing/freeze_policy.html if update is needed"
                     # redirect people to d-i RM for udeb things:
                     if block_cmd == 'block-udeb':
                         tooltip = "please contact the d-i release manager if an update is needed"
