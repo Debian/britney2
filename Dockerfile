822a8c712cf4fb3ab2838208c544d4333ea26d66
Niels Thykier <niels@thykier.net>
2019-01-05 13:42:43 +0000

    Migrate to pycodestyle and setup a basic config file
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM debian:stable
 WORKDIR /britney
 ADD . /britney
-RUN apt-get update && apt-get install --no-install-recommends --assume-yes python3 python3-apt python3-yaml python3-coverage python3-nose python3-pep8 rsync libclass-accessor-perl libdpkg-perl libyaml-syck-perl curl
+RUN apt-get update && apt-get install --no-install-recommends --assume-yes python3 python3-apt python3-yaml python3-coverage python3-nose python3-pycodestyle rsync libclass-accessor-perl libdpkg-perl libyaml-syck-perl curl
--- tests/test_pycodestyle.py
+++ tests/test_pycodestyle.py
@@ -1,12 +1,15 @@
+import os
 import unittest
-import pep8
+import pycodestyle
 
 
 class TestCodeFormat(unittest.TestCase):
 
     def test_conformance(self):
         """Test that we conform to PEP-8."""
-        style = pep8.StyleGuide()
+        project_dir = os.path.dirname(os.path.dirname(__file__))
+        codestyle_cfg = os.path.join(project_dir, 'setup.cfg')
+        style = pycodestyle.StyleGuide(config_file=codestyle_cfg)
         result = style.check_files('.')
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
