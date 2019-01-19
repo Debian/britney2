Debian/britney2

commit 15e52286693641ddbd58a1feae275851bca973fc

Add src_policy option to policies.
The src_policy defines wether, for source items, the source policy should be
run (RUN_SRC, the default), the arch policy should be run on every arch
(RUN_ON_EVERY_ARCH_ONLY), or both (RUN_SRC_AND_EVERY_ARCH).

Signed-off-by: Ivo De Decker <ivodd@debian.org>

britney2/policies/policy.py
@@ -12,7 +12,7 @@ import apt_pkg
 from britney2 import SuiteClass
 from britney2.hints import Hint, split_into_one_hint_per_package
 from britney2.inputs.suiteloader import SuiteContentLoader
-from britney2.policies import PolicyVerdict
+from britney2.policies import PolicyVerdict, ApplySrcPolicy
 from britney2.utils import get_dependency_solvers, compute_item_name
 from britney2 import DependencyType
 
@@ -44,9 +44,15 @@ class PolicyEngine(object):
         suite_class = source_suite.suite_class
         for policy in self._policies:
             if suite_class in policy.applicable_suites:
-                v = policy.apply_src_policy(policy_info, suite_name, src, source_t, source_u, excuse)
-                if v.value > policy_verdict.value:
-                    policy_verdict = v
+                if policy.src_policy.run_arch:
+                    for arch in policy.options.architectures:
+                        v = policy.apply_srcarch_policy(policy_info, suite_name, src, arch, source_t, source_u, excuse)
+                        if v.value > policy_verdict.value:
+                            policy_verdict = v
+                if policy.src_policy.run_src:
+                    v = policy.apply_src_policy(policy_info, suite_name, src, source_t, source_u, excuse)
+                    if v.value > policy_verdict.value:
+                        policy_verdict = v
         excuse.policy_verdict = policy_verdict
 
     def apply_srcarch_policies(self, source_suite, src, arch, source_t, source_u, excuse):
@@ -64,7 +70,7 @@ class PolicyEngine(object):
 
 class BasePolicy(object):
 
-    def __init__(self, policy_id, options, suite_info, applicable_suites):
+    def __init__(self, policy_id, options, suite_info, applicable_suites, src_policy = ApplySrcPolicy.RUN_SRC):
         """The BasePolicy constructor
 
         :param policy_id An string identifying the policy.  It will
@@ -80,6 +86,7 @@ class BasePolicy(object):
         self.options = options
         self.suite_info = suite_info
         self.applicable_suites = applicable_suites
+        self.src_policy = src_policy
         self.hints = None
         logger_name = ".".join((self.__class__.__module__, self.__class__.__name__))
         self.logger = logging.getLogger(logger_name)

britney2/policies/__init__.py
@@ -56,3 +56,33 @@ class PolicyVerdict(Enum):
             PolicyVerdict.REJECTED_CANNOT_DETERMINE_IF_PERMANENT, # Assuming the worst
             PolicyVerdict.REJECTED_PERMANENTLY,
         }
+
+
+@unique
+class ApplySrcPolicy(Enum):
+    """
+    For a source item, run the source policy (this is the default)
+    """
+    RUN_SRC = 1
+    """
+    For a source item, run the arch policy on every arch
+    """
+    RUN_ON_EVERY_ARCH_ONLY = 2
+    """
+    For a source item, run the source policy and run the arch policy on every arch
+    """
+    RUN_SRC_AND_EVERY_ARCH = 3
+
+    @property
+    def run_src(self):
+        return self in {
+            ApplySrcPolicy.RUN_SRC,
+            ApplySrcPolicy.RUN_SRC_AND_EVERY_ARCH,
+        }
+
+    @property
+    def run_arch(self):
+        return self in {
+            ApplySrcPolicy.RUN_ON_EVERY_ARCH_ONLY,
+            ApplySrcPolicy.RUN_SRC_AND_EVERY_ARCH,
+        }

commit 35ae8848b433a9e4c167e657a538316a6ed335e8

Fix apply_src_policies when policy is applied on multiple archs
Move the logic of apply_src_policy and apply_srcarch_policy into PolicyEngine.

This fixes an issue with the excuses.yaml output introduced in commit
15e5228669: only the last verdict was added to the excuse info for that
policy.

Signed-off-by: Ivo De Decker <ivodd@debian.org>

britney2/policies/policy.py
@@ -38,34 +38,47 @@ class PolicyEngine(object):
             policy.save_state(britney)
 
     def apply_src_policies(self, source_suite, src, source_t, source_u, excuse):
-        policy_verdict = excuse.policy_verdict
-        policy_info = excuse.policy_info
+        excuse_verdict = excuse.policy_verdict
         suite_name = source_suite.name
         suite_class = source_suite.suite_class
         for policy in self._policies:
+            pinfo = {}
+            policy_verdict = PolicyVerdict.NOT_APPLICABLE
             if suite_class in policy.applicable_suites:
                 if policy.src_policy.run_arch:
                     for arch in policy.options.architectures:
-                        v = policy.apply_srcarch_policy(policy_info, suite_name, src, arch, source_t, source_u, excuse)
+                        v = policy.apply_srcarch_policy_impl(pinfo, suite_name, src, arch, source_t, source_u, excuse)
                         if v.value > policy_verdict.value:
                             policy_verdict = v
                 if policy.src_policy.run_src:
-                    v = policy.apply_src_policy(policy_info, suite_name, src, source_t, source_u, excuse)
+                    v = policy.apply_src_policy_impl(pinfo, suite_name, src, source_t, source_u, excuse)
                     if v.value > policy_verdict.value:
                         policy_verdict = v
-        excuse.policy_verdict = policy_verdict
+            # The base policy provides this field, so the subclass should leave it blank
+            assert 'verdict' not in pinfo
+            if policy_verdict != PolicyVerdict.NOT_APPLICABLE:
+                excuse.policy_info[policy.policy_id] = pinfo
+                pinfo['verdict'] = policy_verdict.name
+                if policy_verdict.value > excuse_verdict.value:
+                    excuse_verdict = policy_verdict
+        excuse.policy_verdict = excuse_verdict
 
     def apply_srcarch_policies(self, source_suite, src, arch, source_t, source_u, excuse):
-        policy_verdict = excuse.policy_verdict
-        policy_info = excuse.policy_info
+        excuse_verdict = excuse.policy_verdict
         suite_name = source_suite.name
         suite_class = source_suite.suite_class
         for policy in self._policies:
+            pinfo = {}
             if suite_class in policy.applicable_suites:
-                v = policy.apply_srcarch_policy(policy_info, suite_name, src, arch, source_t, source_u, excuse)
-                if v.value > policy_verdict.value:
-                    policy_verdict = v
-        excuse.policy_verdict = policy_verdict
+                policy_verdict = policy.apply_srcarch_policy_impl(pinfo, suite_name, src, arch, source_t, source_u, excuse)
+                if policy_verdict.value > excuse_verdict.value:
+                    excuse_verdict = policy_verdict
+                # The base policy provides this field, so the subclass should leave it blank
+                assert 'verdict' not in pinfo
+                if policy_verdict != PolicyVerdict.NOT_APPLICABLE:
+                    excuse.policy_info[policy.policy_id] = pinfo
+                    pinfo['verdict'] = policy_verdict.name
+        excuse.policy_verdict = excuse_verdict
 
 
 class BasePolicy(object):
@@ -121,16 +134,6 @@ class BasePolicy(object):
         """
         pass
 
-    def apply_src_policy(self, general_policy_info, suite, source_name, source_data_tdist, source_data_srcdist, excuse):
-        pinfo = {}
-        verdict = self.apply_src_policy_impl(pinfo, suite, source_name, source_data_tdist, source_data_srcdist, excuse)
-        # The base policy provides this field, so the subclass should leave it blank
-        assert 'verdict' not in pinfo
-        if verdict != PolicyVerdict.NOT_APPLICABLE:
-            general_policy_info[self.policy_id] = pinfo
-            pinfo['verdict'] = verdict.name
-        return verdict
-
     def apply_src_policy_impl(self, policy_info, suite, source_name, source_data_tdist, source_data_srcdist, excuse):  # pragma: no cover
         """Apply a policy on a given source migration
 
@@ -158,16 +161,6 @@ class BasePolicy(object):
         :return A Policy Verdict (e.g. PolicyVerdict.PASS)
         """
         return PolicyVerdict.NOT_APPLICABLE
-
-    def apply_srcarch_policy(self, general_policy_info, suite, source_name, arch, source_data_tdist, source_data_srcdist, excuse):
-        pinfo = {}
-        verdict = self.apply_srcarch_policy_impl(pinfo, suite, source_name, arch, source_data_tdist, source_data_srcdist, excuse)
-        # The base policy provides this field, so the subclass should leave it blank
-        assert 'verdict' not in pinfo
-        if verdict != PolicyVerdict.NOT_APPLICABLE:
-            general_policy_info[self.policy_id] = pinfo
-            pinfo['verdict'] = verdict.name
-        return verdict
 
     def apply_srcarch_policy_impl(self, policy_info, suite, source_name, arch, source_data_tdist, source_data_srcdist, excuse):
         """Apply a policy on a given binary migration

tests/test_policy.py
@@ -99,15 +99,14 @@ def apply_src_policy(policy, expected_verdict, src_name, *, suite='unstable', ta
     if src_name in suite_info[suite].sources:
         src_u = suite_info[suite].sources[src_name]
         src_t = suite_info.target_suite.sources.get(src_name)
-        _, _, excuse, policy_info = create_policy_objects(src_name)
+        _, _, excuse, _ = create_policy_objects(src_name)
     else:
         src_t, src_u, excuse, policy_info = create_policy_objects(src_name, target_version, source_version)
     suite_info.target_suite.sources[src_name] = src_t
     suite_info[suite].sources[src_name] = src_u
-    verdict = policy.apply_src_policy(policy_info, suite, src_name, src_t, src_u, excuse)
-    pinfo = policy_info[policy.policy_id]
+    pinfo = {}
+    verdict = policy.apply_src_policy_impl(pinfo, suite, src_name, src_t, src_u, excuse)
     assert verdict == expected_verdict
-    assert pinfo['verdict'] == expected_verdict.name
     return pinfo
 
 